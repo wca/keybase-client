c6466969fced4a6ba844c98b67da4ea2c6b7d1d7
Marco Munizaga <marco@marcopolo.io>
2015-09-16 11:39:35 -0700

    Add login component to react native android

--- react-native/Keybase/index.android.js
+++ react-native/Keybase/index.android.js
@@ -10,23 +10,54 @@ var {
   StyleSheet,
   Text,
   View,
+  DrawerLayoutAndroid,
+  TouchableNativeFeedback,
 } = React;
 
+var Login = require('./android-react/login');
+
 var Keybase = React.createClass({
+
+  getInitialState: function() {
+    return {"nav": "intro"};
+  },
+
+  _toLogin: function() {
+    this.refs.drawer.closeDrawer()
+    this.setState({"nav":"login"})
+    console.log("Going to login!")
+  },
+
   render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+      var navigationView = (
+        <View style={{backgroundColor: 'white', flexDirection:"column", position: "absolute", top:0, bottom: 0, right:0, left: 0}}>
+          <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>Im in the Drawer!</Text>
+          <TouchableNativeFeedback onPress={this._toLogin}
+            background={TouchableNativeFeedback.SelectableBackground()}
+            delayPressIn={0}>
+            <View><Text style={{margin: 30}}>Login</Text></View>
+          </TouchableNativeFeedback>
+        </View>
+      );
+
+      var internalView = null;
+
+      if (this.state.nav == "login") {
+        internalView = <Login/>
+      } else {
+        internalView = <Text style={{fontSize: 25, textAlign:"center", marginTop:20}}>Keybase</Text>
+      }
+
+      return (
+        <DrawerLayoutAndroid
+          drawerWidth={200}
+          ref="drawer"
+          drawerPosition={DrawerLayoutAndroid.positions.Left}
+          renderNavigationView={() => navigationView}>
+          {internalView}
+        </DrawerLayoutAndroid>
+      );
+
   }
 });
 
