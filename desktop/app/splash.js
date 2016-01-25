bd6cc71078ac1a9d0efe16aa2989d655a866d835
Marco Munizaga <marco@marcopolo.io>
2016-01-25 12:35:43 -0800

    Fix reference to old helpURL

--- desktop/app/splash.js
+++ desktop/app/splash.js
@@ -1,6 +1,6 @@
 // Checks to see if we've launched before (we touch started.txt). If not, write the file and open a browser
 import {socketRoot, dataRoot, splashRoot} from '../../react-native/react/constants/platform.native.desktop'
-import {helpURL} from '../../react-native/react/constants/urls'
+import {help} from '../../react-native/react/constants/urls'
 import path from 'path'
 
 const filePath = path.join(splashRoot, 'started.txt')
@@ -12,13 +12,12 @@ export default () => {
     if (err) {
       try {
         fs.writeFileSync(filePath, 'This file gets created on first run of the app')
-      }
-      catch (e) {
+      } catch (e) {
         console.error(`Couldn't mark splash screen shown: ${e}`)
         // if we cant' write this file, let's not show the splash, maybe something weird is going on
         return
       }
-      shell.openExternal(helpURL)
+      shell.openExternal(help())
     }
   })
 }
